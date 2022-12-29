21d2ac2dbc430108fb5c55edc3034a2eadd3b14b
Leonardo Moina Hinostroza <leonardo.moina@ibm.com>
2022-12-28 19:09:54 -0500

    Obtener home html

--- src/server/index.js
+++ src/server/index.js
@@ -23,6 +23,10 @@ app.get("/", (req, res) => {
 	res.sendFile(join(__dirname, "../public/index.html"));
 });
 
+app.get("/home", (req, res) => {
+	res.sendFile(join(__dirname, "../public/home.html"));
+});
+
 app.get("/home-route", (req, res) => {
 	res.status(200).json({ message: "mensaje durante el evento"});
 });
